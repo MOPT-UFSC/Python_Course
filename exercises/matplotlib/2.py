064d6466b9ff45928869755f4fde88f678dcb4ce
André Fernandes <fpf.andre@gmail.com>
2025-04-08 17:08:47 -0300

    Add the latex code for every equation from the exercises

--- exercises/matplotlib/2.py
+++ exercises/matplotlib/2.py
@@ -2,6 +2,12 @@ from math import sin, cos, pi
 import matplotlib.pyplot as plt
 
 
+'''
+x(\theta) &= cos(\theta) \\
+y(\theta) &= sin(\theta) \\
+z(\theta) &= \theta
+'''
+
 angles_radians = [pi * angle / 180 for angle in range(0, 360 * 3)]
 x_list = [cos(a) for a in angles_radians]
 y_list = [sin(a) for a in angles_radians]
